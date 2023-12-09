ddc122755f687a9350f68621c098eeb976b9f6c1
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-12-08 23:57:15 -0800

    try urlon to shorten url.

--- storage.js
+++ storage.js
@@ -1,8 +1,14 @@
+
+
 export function getDataFromUrl() {
     const params = new URLSearchParams(window.location.search);
     const data = params.get('data');
     if(data) {
-        return JSON.parse(data);
+        try {
+          return urlon.parse(data);
+        } catch {
+            console.log("Couldn't parse data in query string.")
+        }
     }
     else {
         return null;
@@ -11,7 +17,8 @@ export function getDataFromUrl() {
 
 export function setDataToUrl(data) {
     const params = new URLSearchParams(window.location.search);
-    params.set('data', JSON.stringify(data));
+    params.set('data', urlon.stringify(data));
+    // params.set('data', JSON.stringify(data));
     // params.set('data', 'xyz');
     const newUrl = window.location.origin + window.location.pathname + "?" + params.toString();
     console.log(newUrl);
